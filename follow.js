2b391d4444cdeb3ba0ed9cfc5326669ccb5e4020
Favreyo <monmon25565@gmail.com>
2020-04-01 00:06:29 +0900

    added code

---
+++ follow.js
@@ -0,0 +1,22 @@
+const Twitter = require("twitter");
+const config = require("./config");
+const fs = require("fs").promises;
+const client = new Twitter(config.auth);
+
+
+
+const getFriends = () => new Promise(resolve => {
+  const params = { screen_name: config.screen_name, cursor: -1, stringify_ids: true };
+  client.get("friends/ids", params, ((error, data, response) => {
+    if(error) return console.log(error);
+    resolve(data)
+  }));
+});
+
+async function main() {
+  const friends = await getFriends();
+  await fs.writeFile("follows.json", JSON.stringify(friends));
+
+}
+
+main();
---
+++ main.js
@@ -0,0 +1,25 @@
+const Twitter = require("twitter");
+const fs = require("fs").promises;
+const config = require("./config");
+const client = new Twitter(config.auth);
+
+async function main() {
+  const follows = JSON.parse(await fs.readFile("follows.json"));
+
+  client.get("search/tweets", {q: "#駆け出しエンジニアと繋がりたい", count: 1}, ((error, data, response) => {
+    if (error) return console.log(error);
+    for (const tweet of data.statuses) {
+      if (follows.ids.includes(tweet.user["id_str"])) return;
+
+      client.post("blocks/create", { user_id: tweet.user["id_str"] }, ((error, data, response) => {
+        if(error) return console.log(error);
+
+        const { name, screen_name, description } = data;
+        console.log(`[Blocked] ${name}@${screen_name}: ${description}`);
+      }));
+
+    }
+  }));
+}
+
+main();
