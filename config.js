b31006ab806948745d1a9eb345e526d9f992d208
Favreyo <monmon25565@gmail.com>
2020-04-01 19:16:17 +0900

    formated by prettier

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.auth = {
-  consumer_key: "",
-  consumer_secret: "",
-  access_token_key: "",
-  access_token_secret: ""
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
 };
-exports.screen_name = "";
-exports.blacklist_search_query = [ "#駆け出しエンジニアと繋がりたい", "#今日の積み上げ", "#ブログ書け" ];
+exports.screen_name = '';
+exports.blacklist_search_query = ['#駆け出しエンジニアと繋がりたい', '#今日の積み上げ', '#ブログ書け'];
--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const Twitter = require("twitter");
-const fs = require("fs").promises;
-const config = require("./config");
+const Twitter = require('twitter');
+const fs = require('fs').promises;
+const config = require('./config');
 const client = new Twitter(config.auth);
 
 async function main() {
-  const follows = JSON.parse(await fs.readFile("follows.json"));
+  const follows = JSON.parse(await fs.readFile('follows.json'));
 
   for (const query of config.blacklist_search_query) {
-    client.get("search/tweets", {q: query, count: 100}, ((error, data, response) => {
+    client.get('search/tweets', { q: query, count: 100 }, (error, data, response) => {
       if (error) return console.log(error);
 
       for (const tweet of data.statuses) {
-        if (follows.ids.includes(tweet.user["id_str"])) return;
+        if (follows.ids.includes(tweet.user['id_str'])) return;
 
-        client.post("blocks/create", { user_id: tweet.user["id_str"] }, ((error, data, response) => {
-          if(error) return console.log(error);
+        client.post('blocks/create', { user_id: tweet.user['id_str'] }, (error, data, response) => {
+          if (error) return console.log(error);
 
           const { name, screen_name, description } = data;
           console.log(`[Blocked] ${name}@${screen_name}: ${description}`);
-        }));
+        });
       }
-    }));
+    });
   }
 }
 
