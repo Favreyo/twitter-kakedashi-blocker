b93b1cd671eefd5845ab7e7ca1739874df4c2265
Favreyo <monmon25565@gmail.com>
2020-04-01 19:05:17 +0900

    複数ワードに対応

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ exports.auth = {
   access_token_secret: ""
 };
 exports.screen_name = "";
-exports.blacklist_search_query = "#駆け出しエンジニアと繋がりたい";
+exports.blacklist_search_query = [ "#駆け出しエンジニアと繋がりたい", "#今日の積み上げ", "#ブログ書け" ];
--- main.js
+++ main.js
@@ -6,20 +6,22 @@ const client = new Twitter(config.auth);
 async function main() {
   const follows = JSON.parse(await fs.readFile("follows.json"));
 
-  client.get("search/tweets", {q: config.blacklist_search_query, count: 100}, ((error, data, response) => {
-    if (error) return console.log(error);
-    for (const tweet of data.statuses) {
-      if (follows.ids.includes(tweet.user["id_str"])) return;
+  for (const query of config.blacklist_search_query) {
+    client.get("search/tweets", {q: query, count: 100}, ((error, data, response) => {
+      if (error) return console.log(error);
 
-      client.post("blocks/create", { user_id: tweet.user["id_str"] }, ((error, data, response) => {
-        if(error) return console.log(error);
+      for (const tweet of data.statuses) {
+        if (follows.ids.includes(tweet.user["id_str"])) return;
 
-        const { name, screen_name, description } = data;
-        console.log(`[Blocked] ${name}@${screen_name}: ${description}`);
-      }));
+        client.post("blocks/create", { user_id: tweet.user["id_str"] }, ((error, data, response) => {
+          if(error) return console.log(error);
 
-    }
-  }));
+          const { name, screen_name, description } = data;
+          console.log(`[Blocked] ${name}@${screen_name}: ${description}`);
+        }));
+      }
+    }));
+  }
 }
 
 main();
