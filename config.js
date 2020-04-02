959bf1dc1070a68f2618aeb93cc100a5763bbabe
Favreyo <monmon25565@gmail.com>
2020-04-02 21:05:15 +0900

    add search query

--- config.js
+++ config.js
@@ -4,16 +4,18 @@ exports.auth = {
   access_token_key: '',
   access_token_secret: '',
 };
-exports.screen_name = '';
+
 exports.blacklist_search_query = [
   '#駆け出しエンジニアと繋がりたい',
   '#駆け出しエンジニア',
   '#プログラミング初心者',
+  '#プログラミング学習',
   '#今日の積み上げ',
   '#ブログ書け',
-  '#プログラミング初心者',
   '#フリーランス',
   '#コード書け',
   '#プログラミング学習',
-  '#人生逆転サロン'
+  '#人生逆転サロン',
+  '#TECHBATON',
+  '#ウェブカツ'
 ];
